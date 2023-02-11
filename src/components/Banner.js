d695c2d449b91d4ce266ba81b46967e4179318ca
duckgocloud <linxiaoting100@gmail.com>
2023-02-11 16:30:58 +0100

    practice

---
+++ src/components/Banner.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import hearderImg from '../assets/img/header-img.svg';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ['web developer', 'web Designer', 'UI/UX Designer'];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const period = 2000;
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            tick();
+        },delta)
+
+        return ()=>{clearInterval(ticker)}
+    },[text])
+
+    const tick = ()=>{
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+        setText(updatedText);
+
+        if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+
+    }
+
+    return(
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-item-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <TrackVisibility>
+                        {({ isVisible }) => 
+                            <div className={isVisible ? 'animate__animated animate__fadeIn': ''}>
+                                <span className="tagline">Wecome to my Portfolio</span>
+                                <h1>{`Hi I'm Webdecoded`}<span className="wrap">{text}</span></h1>
+                                <p>hubjvbsjbjnjabj j jnljnjabhvbhbjhbjh</p> 
+                                <button onClick={()=>{ console.log('connect')}}>Let's Connect<ArrowRightCircle size={25}/></button>
+                            </div>}
+                        </TrackVisibility>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={hearderImg} alt='Headder' />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
