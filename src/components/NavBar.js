6c03c74ef1f8f4e49df910f9a4a740948fbb57aa
duckgocloud <linxiaoting100@gmail.com>
2023-02-22 22:59:25 +0100

    contact bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,12 +51,12 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href='#'><img src={navIcon1} alt="" /></a>
+                <a href='https://www.linkedin.com/in/xiaoting-lin-72b794189/' target='_blank'><img src={navIcon1} alt="" /></a>
                 <a href='#'><img src={navIcon2} alt="" /></a>
                 <a href='#'><img src={navIcon3} alt="" /></a>
                 <a href='#'><img src={navIcon4} alt="" /></a>
               </div>
-                {/* <button className="vvd"><span>Let's Connect</span></button> */}
+                <a href="#contact"><button className="vvd"><span>Let's Connect</span></button></a>
             </span>
           </Navbar.Collapse>
         </Container>
