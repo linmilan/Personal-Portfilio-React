511c93b1312d282ac030c48d60fa29f3c3feb747
duckgocloud <linxiaoting100@gmail.com>
2023-05-02 22:08:46 +0200

    tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
 import { Col, Container, Row, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import colorSharp2 from '../assets/img/color-sharp2.png';
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
@@ -96,9 +95,94 @@ export const Projects = () =>{
             imgUrl:projImg11,
             link:'https://github.com/linmilan/Navigation2',
             demo:'https://linmilan.github.io/Navigation2/',
-        },
+        }
         
     ]
+
+    const frontend = [
+        {
+            title: 'Responsive E-commerce Webpage',
+            description:'This is a responsive e-commerce website built with HTML, CSS, and JavaScript, featuring essential functionalities such as product display and shopping cart. It is deployed on GitHub pages and utilizes technologies including jQuery, Bootstrap, Font Awesome, and more. Some functionalities are currently under development.',
+            imgUrl:projImg1,
+            link:'https://github.com/linmilan/Ecommerce-HTML-CSS-JS',
+            demo:'https://linmilan.github.io/Ecommerce-HTML-CSS-JS/'
+        },
+        {
+            title: 'React Notes App',
+            description:'This is a note-taking app created using React that enables users to easily create, edit, and delete notes. It also features a search function to help users find their notes quickly. The app utilizes React components, React Router, and React hooks. It is currently hosted on GitHub Pages.',
+            imgUrl:projImg6,
+            link:'https://github.com/linmilan/REACT-NOTES-APP',
+            demo:'https://linmilan.github.io/REACT-NOTES-APP'
+        },
+        {
+            title: 'Product Landing Page',
+            description:'Design & Development',
+            imgUrl:projImg2,
+            link:'https://github.com/linmilan/Product-Landing-Page',
+            demo:'https://linmilan.github.io/Product-Landing-Page/'
+        }
+    ]
+
+    const smallproj = [
+        {
+            title: 'Awesome Button',
+            description:'Design & Development',
+            imgUrl:projImg3,
+            link:'https://github.com/linmilan/Awesome-Button-Ripple-Effects',
+            demo:'https://linmilan.github.io/Awesome-Button-Ripple-Effects/'
+        },
+        {
+            title: 'Balance Ball',
+            description:'Design & Development',
+            imgUrl:projImg4,
+            link:'https://github.com/linmilan/Balance-Ball',
+            demo:'https://linmilan.github.io/Balance-Ball/'
+        },
+        {
+            title: 'Login Hover Effect',
+            description:'Design & Development',
+            imgUrl:projImg7,
+            link:'https://github.com/linmilan/Login-with-hover-effect',
+            demo:'https://linmilan.github.io/Login-with-hover-effect/'
+        },
+        {
+            title: 'Picasso Painting',
+            description:'Design & Development',
+            imgUrl:projImg8,
+            link:'https://github.com/linmilan/Picasso-painting',
+            demo:'https://linmilan.github.io/Picasso-painting/'
+        },
+        {
+            title: 'Clock',
+            description:'This project utilizes HTML, CSS, and JavaScript . The current time is obtained by instantiating a time object, and the setInterval() function is used to update the clock every second. To facilitate code management and maintenance, common functionalities such as obtaining the current time and setting the pointer rotation angle have been encapsulated into functions.',
+            imgUrl:projImg9,
+            link:'https://github.com/linmilan/Clock',
+            demo:'https://linmilan.github.io/Clock/'
+        },
+        {
+            title: 'Colorful Buttons',
+            description:'Design & Development',
+            imgUrl:projImg10,
+            link:'https://github.com/linmilan/Colorful-buttons-1',
+            demo:'https://linmilan.github.io/Colorful-buttons-1/',
+        },
+        {
+            title: 'Navigation',
+            description:'Design & Development',
+            imgUrl:projImg11,
+            link:'https://github.com/linmilan/Navigation2',
+            demo:'https://linmilan.github.io/Navigation2/',
+        },
+        {
+            title: 'City Skyline',
+            description:'Design & Development',
+            imgUrl:projImg5,
+            link:'https://github.com/linmilan/City-Skyline',
+            demo:'https://linmilan.github.io/City-Skyline/'
+        },
+    ]  
+
+
     return(
         <section className="project" id='projects'>
             <Container>
@@ -138,10 +222,32 @@ export const Projects = () =>{
                                             </Row>
                                         </Tab.Pane>
                                         <Tab.Pane eventKey='second'>
-                                            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p> */}
+                                            <Row>
+                                                {
+                                                    frontend.map((project, index)=>{
+                                                        return(
+                                                            <ProjectCard 
+                                                            key={index}
+                                                            {...project}
+                                                            />
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
                                         </Tab.Pane>
                                         <Tab.Pane eventKey='third'>
-                                            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p> */}
+                                        <Row>
+                                                {
+                                                    smallproj.map((project, index)=>{
+                                                        return(
+                                                            <ProjectCard 
+                                                            key={index}
+                                                            {...project}
+                                                            />
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
                                         </Tab.Pane>
                                     </Tab.Content>
                                 </Tab.Container>
